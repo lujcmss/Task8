2b3b9d5f682e392c4c4c308754ad18ffb59332ed
Cats <Cats881119@gmail.com>
2014-01-19 15:01:24 -0500

    modify forms

--- Task7/src/task7/formbeans/SellFundForm.java
+++ Task7/src/task7/formbeans/SellFundForm.java
@@ -1,7 +1,36 @@
 package task7.formbeans;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.mybeans.form.FormBean;
 
 public class SellFundForm extends FormBean{
+	private String tickerOrFund;
+	public String getTickerOrFund() {
+		return tickerOrFund;
+	}
 
+	public void setTickerOrFund(String s) {
+		this.tickerOrFund= trimAndConvert(s,"<>>\"]");
+	}
+
+	public List<String> getValidationErrors() {
+		List<String> errors = new ArrayList<String>();
+
+		if (tickerOrFund == null || tickerOrFund.length() == 0) {
+			errors.add("Ticker or Fund name is required");
+		}
+
+	
+		if (errors.size() > 0)
+		return errors;
+		
+		if (tickerOrFund.matches(".*[<>\"].*")) errors.add("Ticker or Fund name may not contain angle brackets or quotes");
+		
+		return errors;
+	}
+	
+	
+	
 }
--- Task7/src/task7/formbeans/TransactionDayForm.java
+++ Task7/src/task7/formbeans/TransactionDayForm.java
@@ -3,5 +3,6 @@ package task7.formbeans;
 import org.mybeans.form.FormBean;
 
 public class TransactionDayForm extends FormBean{
-
+//I am not sure since the form will update serveral new price for different fund
+	//how do we distinguish them from easy other? new price1? new price 2?,,
 }
