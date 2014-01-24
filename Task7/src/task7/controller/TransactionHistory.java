dd7b48d35b82746b421e2acb212c5718fd500298
PatricioZ <PatricioZ@128.237.216.199>
2014-01-23 21:33:12 -0500

    transaction DAO and transacation history DAO
    added the select dao, in the transactionDAO, you will find 2 new
    methods.., 
    
    one method calls another method, the reason I am using two methods is
    because I had to select something from one table and then return to the
    original method and use this selection to obtain the required
    informaiton,  so basically , in the original method, the email is being
    passed to it, later within this method, the email is passed to another
    method and this method uses the email to search on the customer table to 
    find the customer id, later when the id is obtained in returns to the
    original method to use this id to find all the transactions from such
    customer.., , if you have any questions on how it workss, call me :)

--- Task7/src/task7/controller/TransactionHistory.java
+++ Task7/src/task7/controller/TransactionHistory.java
@@ -4,27 +4,70 @@ import java.util.ArrayList;
 import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
+import task7.databeans.TransactionBean;
+import task7.model.FundDAO;
 import task7.model.Model;
+import task7.model.TransactionDAO;
 //akshata test 15 jan 530pm
 //pato was here back test
 public class TransactionHistory extends Action {
-
+	private TransactionDAO transactionDAO = new TransactionDAO();
+	private TransactionBean[] transaction ;
 	public TransactionHistory(Model model) {
+		//transactionDAO  = model.getTransactionDAO();
 	}
-
+	
 	public String getName() { return "transactionHistory.do"; }
-
+	
 	public String perform(HttpServletRequest request) {
         // Set up the errors list
         List<String> errors = new ArrayList<String>();
         request.setAttribute("errors", errors);
+        HttpSession session = request.getSession();
+                //Identifying if the user session is created, if its not, then it will return the user
+        //to the login page
+      
+        System.out.println("eneters transaction history");
         
-		try {
+	
+			
+	      //    if (session.getAttribute("email") == null){
+	        //	  errors.add("Not logged in");
+	        	//  return "login.jsp";
+	          //}
+			
+	          //Checking the userType
+	         // if (session.getAttribute("userType") == "Employee"){
+	        	  
+	        	  
+	          //}else{
+	        	 // session.getAttribute("email"); 
+	        	  //It must be displayed by order, i think it will display by default.
+	        	 // String email = (String) (session.getAttribute("email"));
+			 System.out.println("Just before doing dao thing.");
+			 		
+			String email = "df";	
+			//this is just a test, the email shall be collected from the session.
+			transaction = transactionDAO.getTransactionsPerUser(email);
+			//Here we can use a for loop  to get all the variables out if required..
+			System.out.println(transaction[0].getAmount());
+	        	
+	        	  
+	          //}
+	          
+	          
+			
+	        	   
+	          
+	          
+	          
+	        
+			
+			
+			
 	        return "transactionHistory.jsp";
-        } catch (Exception e) {
-        	errors.add(e.getMessage());
-        	return "error.jsp";
-        }
+       
     }
 }
--- Task7/src/task7/model/TransactionDAO.java
+++ Task7/src/task7/model/TransactionDAO.java
@@ -1,6 +1,15 @@
 package task7.model;
-import org.hibernate.Session;
+import java.util.List;
 
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+
+
+import org.hibernate.mapping.Array;
+
+import task7.databeans.CustomerBean;
 import task7.databeans.FundBean;
 import task7.databeans.TransactionBean;
 
@@ -35,6 +44,75 @@ public class TransactionDAO {
 		session.beginTransaction().commit();
 
 
+	}
+	
+	
+	public TransactionBean[] getTransactionsPerUser(String email) {
+		 System.out.println("Amount"+ email);
+		Session session = HibernateUtil.getSessionFactory().openSession();
+		 System.out.println("Amount"+ email);
+		 Transaction tx = null;
+		 TransactionBean[] user = null;
+		 Integer id2 = getId(email);
+		 try {
+			 tx = session.getTransaction();
+			 tx.begin();
+			 Session session2 = HibernateUtil.getSessionFactory().openSession();
+				Query query = session2.createQuery("from TransactionBean where customerBean_customerId =  :id ");
+				query.setParameter("id", id2);
+				List<TransactionBean> personList = query.list();
+				 
+				 TransactionBean[] a = new TransactionBean[personList.size()];
+				 personList.toArray(a);
+				 
+				System.out.println("Data"+personList);
+				
+     	  
+				user  = a;
+				
+			 tx.commit();
+		 } catch (Exception e) {
+			 if (tx != null) {
+				 tx.rollback();
+			 }
+			 e.printStackTrace();
+		 } finally {
+			 session.close();
+		 }
+		 return user;
+	}
+	private int getId(String id) {
+		 System.out.println(id);
+		 int i = 0;
+		Session session = HibernateUtil.getSessionFactory().openSession();
+		 Transaction tx = null;
+		
+		 try {
+			 tx = session.getTransaction();
+			 tx.begin();
+			 Session session2 = HibernateUtil.getSessionFactory().openSession();
+				Query query = session2.createQuery("from CustomerBean where email=  :id ");
+				query.setParameter("id", id);
+				List <?>list = query.list();
+				System.out.println("Data"+list);
+				
+     	  
+				CustomerBean x = (CustomerBean)list.get(0);
+				 i = x.getCustomerId();
+				 System.out.println(x.getCustomerId());
+			 tx.commit();
+		 } catch (Exception e) {
+			 if (tx != null) {
+				 tx.rollback();
+			 }
+			 e.printStackTrace();
+		 } finally {
+			 session.close();
+		 }
+		
+		 return i;
+		
+		
 	}
 	
 	
